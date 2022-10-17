700efdd9f31aad730631cd63107c7d312a03d77e
Nemesis123925 <guoyuxuan1801@gmail.com>
2022-10-17 17:28:25 -0500

    Initial Commit

---
+++ ShadesBusiness/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShadesBusiness
+//
+//  Created by 郭宇轩 on 10/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShadesBusiness/ShadesBusinessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShadesBusinessApp.swift
+//  ShadesBusiness
+//
+//  Created by 郭宇轩 on 10/17/22.
+//
+
+import SwiftUI
+
+@main
+struct ShadesBusinessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
